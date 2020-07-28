94147520eda555065f9f7b1a1f3f48000c16e38b
Xelek <l-baal@yandex.ru>
2020-07-28 16:56:20 +0300

    !

--- include/class/Austenite.class.php
+++ include/class/Austenite.class.php
@@ -3,7 +3,7 @@
 
 class Austenite extends Alloy
 {
-    protected $dislocation_density = 7 * 1e11;
+    protected $dislocation_density = 7e11;
     protected $sigma020 = 67.8;
     protected $hall_petch = 224;
     protected $props  = ['M' => 3.1, 'G' => 8.4e4, 'b' => 2.5e-10];
